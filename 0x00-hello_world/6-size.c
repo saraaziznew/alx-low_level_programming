0b21dab74a14bcae12fcb617463a318740fa875a
saraaziznew <saraaelshaikh@gmail.com>
2023-07-13 18:53:25 -0700

    commit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("size of a char: %lu byte(s)\n", sizeof(a));
-printf("size of an int: %d byte(s)\n", sizeof(b));
-printf("size of a long int: %d byte(s)\n", sizeof(c));
-printf("size of a long long int: %d byte(s)\n", sizeof(d));
-printf("size of a float: %f byte(s)\n", sizeof(f));
+printf("size of a char: %s byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of a float: %f byte(s)\n", sizeof(float));
 return (0);
 }
