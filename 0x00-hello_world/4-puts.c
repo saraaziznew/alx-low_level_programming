21bd321f917c0281b9208b52bf98fb109328a248
saraaziznew <saraaelshaikh@gmail.com>
2023-07-13 18:13:22 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-
+/**
+ * main:A program that prints a line with puts function
+ * Return: 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
