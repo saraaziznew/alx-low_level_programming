71282f20e276351c7be716a483e9a3426e9ac062
saraaziznew <saraaelshaikh@gmail.com>
2023-07-15 20:07:22 -0700

    hi

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
