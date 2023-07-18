e8b69dfcc08e17df683d26b302cbbe59d7e73c4d
saraaziznew <saraaelshaikh@gmail.com>
2023-07-18 07:59:01 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+
+	puts(c);
+	return (0);
+}
