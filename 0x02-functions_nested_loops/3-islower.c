25bfcadff92f55e217222b5abd22ea74e010f616
saraaziznew <saraaelshaikh@gmail.com>
2023-07-18 17:34:10 -0700

    m

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * islowe - function to check the lower case
+ * Return: 0 (success)
+ */
+int _islower(int c)
+{
+	int i;
+	int r;
+
+	for (i = 97; i <= 122; i++)
+		r = islower(i);
+	        if (i == 99)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+}
