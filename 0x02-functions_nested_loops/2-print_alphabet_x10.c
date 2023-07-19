5d2ad4c150f68781d880204e28a449cbbd86a5b1
saraaziznew <saraaelshaikh@gmail.com>
2023-07-18 17:14:37 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,11 @@ void print_alphabet_x10(void)
 	int n;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (n = 97; n < 122; n++)
+		{
 			_putchar(n);
-	_putchar('\n');
+	                _putchar('\n');
+		}
+	}
 }
