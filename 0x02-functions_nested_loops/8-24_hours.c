68d449d30b69ac98b5e47e89412a537feaf0503b
saraaziznew <saraaelshaikh@gmail.com>
2023-07-19 18:08:41 -0700

    m

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minuts of the day
+ */
+void jack_bauer(void)
+{
+	int min, h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
