3df542bcf91db49bb21631a1215c849b832a2172
saraaziznew <saraaelshaikh@gmail.com>
2023-07-19 17:10:31 -0700

    m

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isalpha - function show 1 if the input is a
+ other cases show 0
+ * @c - parameter 
+ * Return: 0 (success)
+ */
+int _isalpha(int c);
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
