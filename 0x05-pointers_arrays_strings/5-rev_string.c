92eac8509c2c371b2bda7b7fb01f9dfc6ea54ac4
saraaziznew <saraaelshaikh@gmail.com>
2023-07-24 16:18:35 -0700

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,9 +11,10 @@ void rev_string(char *s)
 	 
 	for (l = 0; s[l] != '\n'; ++l)
 		;
-	for (n = 0; n < 1 / 2; i++)
+	for (n = 0; n < 1 / 2; n++)
 	{
 		temp = s[n];
 		s[n] = s[l - 1 - n];
 		s[l - 1 - n] = temp;
 	}
+}
