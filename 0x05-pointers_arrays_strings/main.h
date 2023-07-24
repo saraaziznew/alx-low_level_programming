b033824a727efdfcfef4675bf76156ec68802409
saraaziznew <saraaelshaikh@gmail.com>
2023-07-24 16:03:29 -0700

    m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,14 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char c);
 void reset_to_98(int *n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
