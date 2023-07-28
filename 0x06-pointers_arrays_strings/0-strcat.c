c6243f4c7b262ceb7bd246968bd4e9d5ee4be33f
saraaziznew <saraaelshaikh@gmail.com>
2023-07-27 18:27:04 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,10 @@ char *_strcat(char *dest, char *src)
 	int c, c2;
 
 	c = 0;
-	while (dest[0])
+	while (dest[0] != '\0')
 		c++;
 
-	for (c2 = 0; src[c2]; c2++)
+	for (c2 = 0; src[c2] != '\0'; c2++)
 		dest[c++] = src[c2];
 
 	return (dest);
