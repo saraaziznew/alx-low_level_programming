bf71f2ab4dae1a1442e40c3e28c23bdd07ecbe37
saraaziznew <saraaelshaikh@gmail.com>
2023-07-31 17:03:49 -0700

    m

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - entry point
+ * @a: string to print
+ * @size: string to print
+ * Return: 0 (success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d ", s1);
+	printf("%d\n", s2);
+}
