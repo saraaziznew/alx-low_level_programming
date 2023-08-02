eb4e644697a4d4d428e1ae3bb232e19d69a80701
saraaziznew <saraaelshaikh@gmail.com>
2023-08-02 15:05:32 -0700

    m

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,8 +10,12 @@ int factorial(int n)
 	{
 		return (-1);
 	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
 	else
 	{
-		return (n * factorial( n - 1));
+		return (n * factorial ( n - 1));
 	}
 }
