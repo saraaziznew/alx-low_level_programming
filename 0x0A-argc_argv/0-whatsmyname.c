e5afca789e960046de0c53267fbed967c3de7bb0
saraaziznew <saraaelshaikh@gmail.com>
2023-08-04 11:36:31 -0700

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * @argc: string to print
+ * @argv: string to print
+ * Return: 0 (success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
