732276515abc3197c046cf7e1f386df6b4d00635
saraaziznew <saraaelshaikh@gmail.com>
2023-08-11 12:17:19 -0700

    m

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 	return (ptr);
 }
 /**
- * *_calloc - entry point
+ * *_calloc - allocates memory for an arra,using malloc
  * @nmemb: array length
  * @size: size of each element
  * Return: pointer
