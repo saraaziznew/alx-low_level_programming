05dc1868f32bd785f3164bad5a8eefb273fb4c63
saraaziznew <saraaelshaikh@gmail.com>
2023-08-14 21:26:11 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+/**
+ * struct dog - new type that hold deffirnt data type
+ * @name: string to print
+ * @owner: string to print
+ * @age: string to print
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
