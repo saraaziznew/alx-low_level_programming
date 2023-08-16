b66555022abbf7e7746ebf5611228bffe33f4bc1
saraaziznew <saraaelshaikh@gmail.com>
2023-08-15 18:54:41 -0700

    m

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * free_dog - frees your dows
+ * @d : string
+ * Return: 0 (success)
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
