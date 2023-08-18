b1a532c8279a1861421014412621fa78b400ab93
saraaziznew <saraaelshaikh@gmail.com>
2023-08-18 14:54:24 -0700

    m

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,72 @@
+#include "variadic_functions.h"
+/**
+ * format_char - fomat char
+ * @separator: string to print
+ * @ap:string to print
+ * Return: 0 (success)
+ */
+void format_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+			}
+/**
+ * format_int - fomat char
+ * @separator: string to print
+ * @ap:string to print
+ * Return: 0 (success)
+ */
+void format_int(char *separator, va_list ap)
+{
+printf("%s%d", separator, va_arg(ap, int));
+		}
+		/**
+ * format_string - format string
+ * @separator: string to print
+ * @ap:string to print
+ * Return: 0 (success)
+ */
+		void format_string(char *separator, va_list ap)
+		{
+		char *str = va_arg(ap, char *);
+
+		switch ((int)(!str))
+			case 1:
+		str = "(nil)";
+		printf("%s%s", separator, str);
+		}
+		/**
+ * print_all - entry point
+ * @format:stirng to print
+ * @...:string to print
+ * Return: 0 (success)
+ */
+void print_all(const char * const format, ...)
+{
+int i = 0, j;
+char *separator = "";
+va_list ap;
+token_t tokens[] = {
+	{"c", format_char},
+	{"i", format_int},
+	{"f", format_int},
+	{"s", format_string},
+	{NULL, NULL}
+};
+va_start(ap, format);
+while (format && format[i])
+{
+	j = 0;
+	while (tokens[j].token)
+	{
+		if (format[i] == tokens[j].token[0])
+				{
+				tokens[j].f(separator, ap);
+				separator = ", ";
+				}
+				j++;
+				}
+				i++;
+				}
+				printf("\n");
+				va_end(ap);
+				}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,16 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * struct token - struct token
+ * @token: string
+ * @f:strinr
+ */
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+}token_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
