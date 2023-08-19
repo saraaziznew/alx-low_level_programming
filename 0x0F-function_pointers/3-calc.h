26916b7ab5c575e456562728227b42d68c5254c4
saraaziznew <saraaelshaikh@gmail.com>
2023-08-18 17:37:14 -0700

    m

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -4,10 +4,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct op - struct op
+ * @op :string to pri
+ * @f: string to
+ */
 typedef struct op
 {
-        char *op;
-        int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
