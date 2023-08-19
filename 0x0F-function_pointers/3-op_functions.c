653cbbebecef833b7bc4377929eec0bf79a5aee2
saraaziznew <saraaelshaikh@gmail.com>
2023-08-18 17:25:40 -0700

    m

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adda 2 integer
+ * @a:string to print
+ * @b:string to print
+ * Return: 0 (success)
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts 2 integer
+ * @a: string to print 
+ * @b: string to print 
+ * Return: 0 (success)
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * op_mul - multiple 2 integer
+ * @a: string to print
+ * @b: string to print
+ * Return: 0 (success)
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * op_div - divided 2 integer
+ * @a: string to print
+ * @b: string to print
+ * Return: 0 (success)
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * op_mod - modulo 2 integer
+ * @a: string to print
+ * @b: string to print
+ * Return: 0 (success)
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
