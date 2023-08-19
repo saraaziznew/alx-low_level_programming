9d7aa5df5c3a402aa80d6de2b5f1610303bda49e
saraaziznew <saraaelshaikh@gmail.com>
2023-08-18 17:44:38 -0700

    m

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,13 +11,13 @@ int op_add(int a, int b)
 }
 /**
  * op_sub - subtracts 2 integer
- * @a: string to print 
- * @b: string to print 
+ * @a: string to print
+ * @b: string to print
  * Return: 0 (success)
  */
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 /**
  * op_mul - multiple 2 integer
@@ -27,7 +27,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 /**
  * op_div - divided 2 integer
@@ -37,7 +37,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-        return (a / b);
+	return (a / b);
 }
 /**
  * op_mod - modulo 2 integer
@@ -47,5 +47,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-        return (a % b);
+	return (a % b);
 }
