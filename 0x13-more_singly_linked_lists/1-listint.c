d4b87b1be0baeefc95c9be5b5e79a29d261cf5f1
saraaziznew <saraaelshaikh@gmail.com>
2023-08-29 10:00:12 -0700

    m

---
+++ 0x13-more_singly_linked_lists/1-listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * size_t listint_len - to print the integer
+ * @h: struct to print
+ * Return: 0 (success)
+ */
+size_t listint_len(const listint_t *h)
+{
+        const listint_t *head = NULL;
+        int i = 0;
+
+        head = h;
+        if (head == NULL)
+        {
+                ;
+        }
+        while (head != NULL)
+        {
+                i++;
+                head = head->next;
+        }
+        return (i);
+}
