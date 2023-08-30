45c3d604416030b4bada5d2e88a7fdbb54e748cd
saraaziznew <saraaelshaikh@gmail.com>
2023-08-30 16:30:55 -0700

    m

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * *add_nodeint_end - print in reverse
+ * *add_nodeint - print in reverse
  * @head: string to print
  * @n: string to print
  * Return: 0 (success)
@@ -8,15 +8,15 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
-	
+
 	if (!head || !new)
 	{
 		return (NULL);
 	}
-	
+
 	new->next = NULL;
 	new->n = n;
-	
+
 	if (*head)
 	{
 		new->next = *head;
