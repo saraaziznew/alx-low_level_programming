62bdbbbe5afe034b81adfded6d5aafbdaee9d3aa
saraaziznew <saraaelshaikh@gmail.com>
2023-09-01 18:14:25 -0700

    m

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * clear_bit - clear the value of bit
+ * @n:string to print
+ * @index: string to print
+ * Return: 0 (success)
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	if (*h & 1l << index)
+		*n ^= 1l << index;
+	return (1);
+}
