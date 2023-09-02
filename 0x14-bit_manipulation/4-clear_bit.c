09be7d3b88c3d98b112ab35f2705df7534d560f2
saraaziznew <saraaelshaikh@gmail.com>
2023-09-01 18:17:06 -0700

    m

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	if (*h & 1l << index)
+	if (*n & 1l << index)
 		*n ^= 1l << index;
 	return (1);
 }
