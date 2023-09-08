479c8dc289970d4e71a67c05fd2827ce2d8d7bf6
saraaziznew <saraaelshaikh@gmail.com>
2023-09-08 16:20:20 -0700

    m

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,11 @@
 #define main_h
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define READ_BUF_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
