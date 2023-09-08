39c6aea05f63bdb18b4ed098f8f5cf2b99515502
saraaziznew <saraaelshaikh@gmail.com>
2023-09-08 16:39:57 -0700

    m

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: strint to print
+ * Return: 0 (success)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * create_file - function to craete the file
+ * @filename: string to string
+ * @text_content: string to print
+ * Return: 0 (success)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
