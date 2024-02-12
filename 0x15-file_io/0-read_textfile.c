17ae0263f202b72a8e59c55d12019d28afb169f0
sara aziz <saraaelshaikh@gmail.com>
2024-02-12 15:52:42 -0800

    hi

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,10 +12,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
-	if(!filename || !letters)
+	if (!filename || !letters)
 		return(0);
 	fp = open(filename,O_RDONLY);
-	if(fp == -1)
+	if (fp == -1)
 		return(0);
 	bytes = read(fp , &buf[0] , letters);
 	bytes = write(STDOUT_FILENO, &buf[0] , bytes);
