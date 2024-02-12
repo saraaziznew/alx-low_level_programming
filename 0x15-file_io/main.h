5f7d1865aef5f8bf2a63f22dfde7330819bbc7ec
sara aziz <saraaelshaikh@gmail.com>
2024-02-12 15:56:14 -0800

    hi

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
@@ -8,9 +8,9 @@
 
 #define READ_BUF_SIZE 1024
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
 
 #endif
