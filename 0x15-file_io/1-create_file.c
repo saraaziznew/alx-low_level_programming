7c6ff9c6133c406fb7ebc9c0b3e35ff427d6999b
sara aziz <saraaelshaikh@gmail.com>
2024-02-12 16:36:48 -0800

    hi

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	  if (!filename)
 		  return (-1);
-	  fp = open(filename, 0_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	  fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	  if (fp == -1)
 		  return (-1);
 	  if (len)
