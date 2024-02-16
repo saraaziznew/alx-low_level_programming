221bbfd673ed4a30c41bb7a6b552a8be5b79111c
sara aziz <saraaelshaikh@gmail.com>
2024-02-16 10:46:00 -0800

    hi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't raed from file %s \n"
-#define ERR_NOWRITE "Error: Can't write to %s \n"
-#define ERR_NOCLOSE "Error: Can't close fd %d \n"
+#define ERR_NOREAD "Error: Can't raed from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR |S_IRGRP | S_IWGRP | S_IROTH)
 
 
